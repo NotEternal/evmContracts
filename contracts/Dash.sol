d1ba13c4f872b0f03b86e901c6cf2fd982084ca5
NotEternal <cent1pede@protonmail.ch>
2022-02-25 15:24:30 +0400

    Update a Dash contract

--- contracts/Dash.sol
+++ contracts/Dash.sol
@@ -2,73 +2,48 @@
 pragma solidity ^0.8.12;
 
 import "./interfaces/IERC20.sol";
+import "./interfaces/IStorage.sol";
 
-contract DashV1 {
-    struct Data {
-        address owner;
-        mapping(string => uint) products;
-    }
-
+contract Dash {
+    address storageAddress;
     address owner;
     address paymentDestination;
     address rewardToken;
     uint rewardAmount;
-    mapping(address => Data) userData;
-    address[] public users;
 
-    modifier onlyOwner(address _account) {
-        require(userData[_account].owner != address(0), "NO_OWNER");
-        require(msg.sender == userData[_account].owner, "FORBIDDEN");
-        _;
-    }
-
-    constructor(address _owner, address _paymentDest, address _rewardToken, uint _rewardAmount) {
+    constructor(address _storageAddress, address _owner, address _paymentDest, address _rewardToken, uint _rewardAmount) {
+        storageAddress = _storageAddress;
         owner = _owner;
         paymentDestination = _paymentDest;
         rewardToken = _rewardToken;
         rewardAmount = _rewardAmount;
     }
 
-    // function getData(address _account) public view returns(Data memory) {
-    //     return userData[_account];
-    // }
+    function getData(string memory _key) public view returns(IStorage.Data memory) {
+        return IStorage(storageAddress).getData(_key);
+    }
 
-    function payment(address _account, string memory _productId) external payable {
+    function payment(string memory _key, string memory _data, address _rewardReceiver) external payable {
         require(msg.value > 0, "EMPTY_VALUE");
-        if (userData[_account].owner != address(0)) {
-            require(msg.sender == userData[_account].owner, "FORBIDDEN");
-        } else {
-            users.push(_account);
-        }
         (bool success,) = paymentDestination.call{value: msg.value}("");
         require(success, "FAILED_PAYMENT");
-        setData(_account, _productId);
+        setData(_key, _data);
         if (IERC20(rewardToken).balanceOf(address(this)) >= rewardAmount) {
-            sendReward(_account);
+            sendReward(_rewardReceiver);
         }
     }
 
-    function cleanData(address _account) public onlyOwner(_account) {
-        require(msg.sender == userData[_account].owner, "FORBIDDEN");
-        delete userData[_account];
-        bool arrayOffset;
-        for(uint x; x < users.length - 1; x++) {
-            if (keccak256(abi.encodePacked(users[x])) == keccak256(abi.encodePacked(_account))) {
-                arrayOffset = true;
-            }
-            if (arrayOffset) users[x] = users[x + 1];
-        }
-        if (arrayOffset) users.pop();
+    function clearData(string memory _key) public {
+        IStorage(storageAddress).clearData(_key);
     }
 
-    function setData(address _account, string memory _productId) private {
-        require(msg.sender == userData[_account].owner, "FORBIDDEN");
-        userData[_account].products[_productId] = block.timestamp;
+    function setData(string memory _key, string memory _data) private {
+        IStorage(storageAddress).setData(_key, IStorage.Data({owner: address(this), info: _data}));
     }
 
-    function sendReward(address _account) private {
+    function sendReward(address _to) private {
         require(IERC20(rewardToken).balanceOf(address(this)) >= rewardAmount, "INSUFFICIENT_FUNDS");
-        bool success = IERC20(rewardToken).transferFrom(address(this), _account, rewardAmount);
+        bool success = IERC20(rewardToken).transferFrom(address(this), _to, rewardAmount);
         require(success, "FAILED_REWARD");
     }
 
@@ -80,5 +55,10 @@ contract DashV1 {
     function setPaymentDestination(address _moneyDest) public {
         require(msg.sender == owner, "FORBIDDEN");
         paymentDestination = _moneyDest;
+    }
+
+    function setStorage(address _storageAddress) public {
+        require(msg.sender == owner, "FORBIDDEN");
+        storageAddress = _storageAddress;
     }
 }
